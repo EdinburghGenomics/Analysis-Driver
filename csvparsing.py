3c5d84fd2db679aaf2f5f209497059c92ff41bfa
Luis Cebamanos <l.cebamanos@epcc.ed.ac.uk>
2015-04-03 12:38:58 +0100

    csvparsing.py,xmlparsing.py: Created functions and unit test

--- csvparsing.py
+++ csvparsing.py
@@ -1,41 +1,54 @@
-import os,sys
-import codecs,csv
 
+# reads the sampleIds and sampleNames 
+# Input: Full path to SampleSheet.csv
+# Outout: It returns to lists
+#         1 - list of sampleIds
+#         2-  list of sampleNames
+
+
+def readSampleSheet(filename):
+     
+     import os,sys
+     import codecs,csv
+
+     csvlist=[]
+     sampleId=[]
+     sampleName=[]
+     sampleProject=[]
+
+
+     # read the whole CSV file
+     try:
+          with codecs.open(filename, "rb") as f_obj:
+               reader = csv.reader(f_obj)
+               for row in reader:
+                    csvlist.append(row)
+                    
+     except IOError:
+          print "No such file : ", filename
+          print "Please, introduce a valid path to SampleSheet.csv file"
+          sys.exit()
+          
+      #find [Data] tag
+     dataTagPos = 0
+     for i in xrange(10,len(csvlist)):
+          if csvlist[i][0] == '[Data]':
+               dataTagPos = i
+                 
+       #First row of reald data
+     dataPos=dataTagPos + 2
+
+       # store values in lists
+     for i in xrange(dataPos,len(csvlist)):
+          sampleId.append(csvlist[i][1])
+          sampleName.append(csvlist[i][2])
+          sampleProject.append(csvlist[i][7])
+
+     return sampleId,sampleName
+
+
+# Unit Test
 filename = "/home/U008/lcebaman/scripts/data/SampleSheet.csv"
 print filename
+sampleId,sampleName = readSampleSheet(filename)
 
-
-csvlist=[]
-sampleId=[]
-sampleName=[]
-sampleProject=[]
-
-
-# read the whole CSV file
-try:
-     with codecs.open(filename, "rb") as f_obj:
-          reader = csv.reader(f_obj)
-          for row in reader:
-               csvlist.append(row)
-               
-except IOError:
-     print "No such file : ", filename
-     print "Please, introduce a valid CSV file"
-     sys.exit()
-
-#find [Data] tag
-dataTagPos = 0
-for i in xrange(10,len(csvlist)):
-     if csvlist[i][0] == '[Data]':
-          dataTagPos = i
-
-#First row of reald data
-dataPos=dataTagPos + 2
-
-
-for i in xrange(dataPos,len(csvlist)):
-     sampleId.append(csvlist[i][1])
-     sampleName.append(csvlist[i][2])
-     sampleProject.append(csvlist[i][7])
-
-print sampleName, sampleProject
--- xmlparsing.py
+++ xmlparsing.py
@@ -1,21 +1,35 @@
-import xml.etree.ElementTree as ET
-import os,sys
 
+
+# reads the values needed to create the mask.
+# Input: Full path to RunInfo.xml
+# Outout: Mask read from RunInfo.xml. The mask is made of pair values which indicate:
+#         1 - NumCycles
+#         2-  IsIndexedRead
+
+def getMask(filename):
+
+     import xml.etree.ElementTree as ET
+     import os,sys
+
+     # List of information required to be used as the mask in bcl2fastq
+     # Groups of two elements: NumCyles and IsIndexedRead will be stored consecutively
+     mask=[]
+     
+     # get the tree of the XML file
+     tree = ET.parse(file_path).getroot()
+     # we are only intrested in the the Reads section
+     object = tree.find('Run/Reads')
+     #loop over all child elements of Reads storing(in order) NUmCycles and IsIndexedRead info
+     for i in  object.getchildren():
+          mask.append(i.get('NumCycles'))
+          mask.append(i.get('IsIndexedRead'))
+     
+     return mask
+
+# Unit Test
 file_path = "/home/U008/lcebaman/scripts/data/RunInfo.xml"
 print file_path
-#List of information required to be used as the mask in bcl2fastq
-#Groups of two elements: NumCyles and IsIndexedRead will be stored consecutively
-mask=[]
-# get the tree of the XML file
-tree = ET.parse(file_path).getroot()
-# we are only intrested in the the Reads section
-object = tree.find('Run/Reads')
-#loop over all child elements of Reads storing(in order) NUmCycles and IsIndexedRead info
-for i in  object.getchildren():
-     mask.append(i.get('NumCycles'))
-     mask.append(i.get('IsIndexedRead'))
 
+mask = getMask(file_path)
 
-
-
-
+print mask
