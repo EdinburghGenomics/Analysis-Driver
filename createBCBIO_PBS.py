781f053e1cdf47f653ed81e5f57093b0489ed348
Luis Cebamanos <l.cebamanos@epcc.ed.ac.uk>
2015-04-06 13:41:29 +0100

    createBCBIO_PBS.py: Added input names as parameters and generate project instructions

--- createBCBIO_PBS.py
+++ createBCBIO_PBS.py
@@ -1,9 +1,12 @@
 #!/opt/anaconda/bin/python
 
-# 
-def createBCBIO_PBS(i):
+# INPUT:
+#      1- i: Project and job number (total of 36-1 job numbers)
+#      2- fastq1: First file of pair fastq files
+#      3- fastq2: Second pair of pair fastq files
+def createBCBIO_PBS(i, fastq1, fastq2):
     # create a PBS script to run BCL2FASTQ
-    filename="pbs/runBCBIO_"+`i`+".pbs"
+    filename="pbs/runBCBIO"+`i`+".pbs"
     fo = open(filename, "wb")
 
     fo.write("#!/bin/bash\n");
@@ -23,16 +26,29 @@ def createBCBIO_PBS(i):
     fo.write("#PBS -j oe \n");
 
     # output file name
-    stringName ="#PBS -o bcbio_"+`i`
+    stringName ="#PBS -o bcbio"+`i`
 
     fo.write(stringName);
-    fo.write("\n");
+    fo.write("\n\n");
 
     # working directory
-    fo.write("cd $PBS_O_WORKDIR\n");
+    fo.write("cd $PBS_O_WORKDIR\n\n");
 
+    # path to BCBIO
+    BCBIO="/home/U008/lcebaman/bcbio/bin/bcbio_nextgen.py"
+    PROJECT_PATH = "/home/U008/lcebaman/scripts/pbs"
+    PROJECT_NAME = "project"+`i`
+    PROJECT = PROJECT_PATH+"/"+PROJECT_NAME
+    PROJECT_FLAGS="-w template gatk-variant"
+    PROJECT_RUN = BCBIO +" "+ PROJECT_FLAGS +" "+ PROJECT_NAME +" "+ fastq1 +" "+ fastq2
+    
+    # generate project to run bcbio
+    fo.write(PROJECT_RUN);
+    fo.write("\n\n");
     # bash command to run bcl2fastq
-    fo.write("bcbio_nextq ......");
+    BCBIO_RUN = BCBIO +" "+ PROJECT_NAME + "/config/"+PROJECT_NAME+".yaml" + " -n 16 " + "--workdir" +" "+ PROJECT_NAME+"/work"
+    fo.write(BCBIO_RUN);
+    fo.write("\n");
 
     # close the PBS script
     fo.close()
@@ -40,6 +56,9 @@ def createBCBIO_PBS(i):
 
 #Unit Testing
 # creates files from 0 to 2-1
+FASTQ_PATH="/scratch/U008/lcebaman/fastq/1"
+FASTQ1 = FASTQ_PATH+"/1--NA12878-OD1_1.fastq"
+FASTQ2 = FASTQ_PATH+"/1--NA12878-OD1_2.fastq"
 for i in xrange(0,2):
-    createBCBIO_PBS(i)
+    createBCBIO_PBS(i, FASTQ1, FASTQ2 )
 
