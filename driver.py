f75ca276d43f732ea8d6eb2c6402c240b50e60fd
Luis Cebamanos <l.cebamanos@epcc.ed.ac.uk>
2015-04-07 12:53:07 +0100

    driver.py: qsub is now called from driver

--- driver.py
+++ driver.py
@@ -3,43 +3,56 @@
 #This program expects an input directory. From there, we will run BCL2FASTQ and BCBIO
 import sys, getopt
 import os
+sys.path.append('imports')
+
 from xmlparsing import getMask
 from csvparsing import readSampleSheet
 from createBCL2FASTQ_PBS import generateMask,bcl2fastq_PBS
-def parsArgs(argv):
-   inputfile = ''
- 
-   try:
-      opts, args = getopt.getopt(argv,"hi:",["ifile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> '
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> '
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-         assert os.path.exists(inputfile), "Non existing directory, "+ str(inputfile)
-      else:
-           print 'test.py -i <inputfile> '
-           sys.exit()
+from createBCBIO_PBS import bcbio_loop
+from subprocess import call
+from qsub_dep import qsub,qsub_dependents
+from args import parsArgs
 
-   print 'Input directory is', inputfile
-   return inputfile
 
 if __name__ == "__main__":
-   # parse the input directory
-   inputDirectory = parsArgs(sys.argv[1:])
-   #print inputDirectory
    
-   # Read RunInfo.xml
-   mask = getMask(inputDirectory)
-   # print mask
+    # parse the input directory
+    inputDirectory = parsArgs(sys.argv[1:])
+    # print inputDirectory
+    
+    # create pbs temporary directory
+    if not os.path.exists("pbs"):
+        os.makedirs("pbs")
+    
+    # Read RunInfo.xml
+    mask = getMask(inputDirectory)
+    # print mask
 
-   # Read SampleSheet.csv
-   sampleId,sampleName = readSampleSheet(inputDirectory)
-   # print sampleId,sampleName
+    # Read SampleSheet.csv
+    sampleId,sampleName = readSampleSheet(inputDirectory)
+    # print sampleId,sampleName
 
-   # Create BCL2FASTQ PBS script
-   bcl2fastq_PBS(mask)
+    # Create BCL2FASTQ PBS script
+    bcl2fastq_PBS(mask)
+
+    # create BCBIO PBS scripts
+    # Total number of runs
+    n = 2
+    bcbio_loop(n)
+    # get into pbs directory
+    os.chdir("pbs")
+    # submit bcl2fastq 
+    # create a list with the name of the PBS script
+    args=['runBCL2FASTQ.pbs']
+
+    # submit the BCL2FASTQ script to bach scheduler
+    BCL2FASTQ_jobid = qsub_dependents(args)
+
+
+    # submit set of BCBIO jobs
+    for i in range(0, n):
+       scriptName = "runBCBIO" +`i`+".pbs"
+       args=[scriptName]
+       jobid2 = qsub_dependents(args,BCL2FASTQ_jobid)
+
+    
