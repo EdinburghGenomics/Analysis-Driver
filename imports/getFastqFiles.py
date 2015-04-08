354f596f27c3f7eb453db77492d2befdd9372dfa
Luis Cebamanos <l.cebamanos@epcc.ed.ac.uk>
2015-04-08 12:28:05 +0100

    getFastqFiles.py: Added exceptions and assertion to the directory path

--- imports/getFastqFiles.py
+++ imports/getFastqFiles.py
@@ -1,11 +1,15 @@
 # Return a list with the names of fastq files
 # within the given directory
 def getFastqFiles(path):
-    import os
+    import os,sys
     # TODO: Check the paths exists
+    try:
+        assert ( os.path.isdir(path) == True)
+    except AssertionError:
+        print "Expected a valid directory to find fastq files"
+        sys.exit()
     listNames=[]
     for file in os.listdir(path):
         if file.endswith(".fastq"):
             listNames.append(file)
-
     return listNames
