c2d200b5b5827ec64f5a72d41c6513891ae6277d
Luis Cebamanos <l.cebamanos@epcc.ed.ac.uk>
2015-04-29 15:42:49 +0100

    createBCBIO_PBS.py: sampleProject is added as parameter to the functions. It now creates the path according to sampleProject

--- imports/createBCBIO_PBS.py
+++ imports/createBCBIO_PBS.py
@@ -6,13 +6,14 @@
 #      2- v: values -> tuple (Lane,Position,sampleName)
 #      3- inputDirectory: Path to input directory
 #      4- projectName: Name of the project
-
-def bcbio_PBS( k, v, inputDirectory, projectName):
+#      5- sampleProject: Sample_Project 
+def bcbio_PBS( k, v, inputDirectory, projectName, sampleProject):
 
     # create a PBS script to run BCL2FASTQ
     sampleName = str(v[1])    
     lane = str(v[0])
     pos = str(v[2])
+    sampleID=str(k)
 
     filename=projectName+"/pbs/runBCBIO_"+lane+".pbs"
     fo = open(filename, "wb")
@@ -45,26 +46,19 @@ def bcbio_PBS( k, v, inputDirectory, projectName):
     
     # base path to the BCL output
     # TODO: check the output contains "/Data/Intensities/BaseCalls"
-    BASE_PATH= inputDirectory+"/Data/Intensities/BaseCalls"
+    BASE_PATH= inputDirectory +"/"+ sampleProject+"/"+sampleID  #+"/Data/Intensities/BaseCalls"
     
-    # get path to fastq pairs
-    FASTQ = BASE_PATH
-    #    for j in xrange(0,len(v)):
-        # check if sampleName != sampleId and if sampleName exists
-    FASTQ = BASE_PATH+"/"+str(sampleName)+"/"+str(k)
-    FASTQ1 =FASTQ+"/"+str(sampleName)+"_S"+str(pos)+"_L00"+str(lane)+"_R1_001"
-    FASTQ2 =FASTQ+"/"+str(sampleName)+"_S"+str(pos)+"_L00"+str(lane)+"_R2_001"
+    FASTQ1 = BASE_PATH +"/"+str(sampleName)+"_S"+str(pos)+"_L00"+str(lane)+"_R1_001"
+    FASTQ2 = BASE_PATH +"/"+str(sampleName)+"_S"+str(pos)+"_L00"+str(lane)+"_R2_001"
                 
     fo.write("\n\n");
     
     # path to BCBIO
 
     BCBIO="/home/U008/lcebaman/bcbio/bin/bcbio_nextgen.py"
-    PROJECT_PATH = "/home/U008/lcebaman/scripts/pbs"
-    PROJECT_NAME = "project"+lane
-    PROJECT = PROJECT_PATH+"/"+PROJECT_NAME
     projFlags ="-w template gatk-variant"
-    PROJECT_RUN = BCBIO +" "+ projFlags +" "+ sampleName +"_"+lane +" "+ FASTQ1+".fastq.gz" +" "+ FASTQ2+".fastq.gz"
+    PROJECT_RUN = BCBIO +" "+ projFlags +" "+ sampleName +"_"+lane +" "+ FASTQ1+".fastq.gz" +" "+ \
+        FASTQ2+".fastq.gz"
      
     # generate project to run bcbio
     fo.write(PROJECT_RUN);
@@ -72,6 +66,7 @@ def bcbio_PBS( k, v, inputDirectory, projectName):
      # bash command to run bcl2fastq
     BCBIO_RUN = BCBIO +" "+ sampleName+"_"+lane + "/config/"+sampleName+"_"+lane+".yaml" + " -n 16 " + \
         "--workdir" +" "+ sampleName+"_"+lane +"/work"
+
     fo.write(BCBIO_RUN);
     fo.write("\n");
      
@@ -79,7 +74,7 @@ def bcbio_PBS( k, v, inputDirectory, projectName):
     fo.close();
 
 
-def bcbio_loop(d, inputDirectory, projectName):
+def bcbio_loop(d, inputDirectory, projectName, sampleProject):
 
     # number of different PBS scripts
     BASE_PATH= inputDirectory+"/Data/Intensities/BaseCalls"
@@ -87,7 +82,7 @@ def bcbio_loop(d, inputDirectory, projectName):
     for k, v in d.items():
         for j in v:
             # generate a PBS script per n
-            bcbio_PBS( k, j, inputDirectory, projectName)
+            bcbio_PBS( k, j, inputDirectory, projectName, sampleProject)
               
 
 
