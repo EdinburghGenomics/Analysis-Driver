22203983e56bccb36cb72c9a564d25335e3bfc7d
Murray Wham <murray.wham@ed.ac.uk>
2015-07-08 15:21:08 +0100

    #5: Adding yaml parsing to config.py

--- config.py
+++ config.py
@@ -1,9 +1,23 @@
 import os
+import yaml
 
 home = os.getenv('HOME')
+user_config_file = os.path.join(home, '.analysisdriver.yaml')
+local_config_file = os.path.join(os.path.dirname(__file__), 'example_analysisdriver.yaml')
 
-fastq = os.path.join(home, 'fastq')
-jobs = os.path.join(home, 'jobs')
+if os.path.exists(user_config_file):
+    config_file = user_config_file
+else:
+    config_file = local_config_file
 
-job_execution = None  # 'pbs' 'local' None
+run_env = os.getenv('ANALYSISDRIVERENV')
+if not run_env:
+    run_env = 'testing'
+
+config = yaml.load(open(config_file, 'r'))[run_env]
+
+fastq = config['shared']['fastq']
+jobs = config['shared']['jobs']
+
+job_execution = config['analysisdriver']['job_execution']
 
