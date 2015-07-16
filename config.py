b09621b8cd1e9f9c9d48b238601cf587f3437422
tcezard <timothee.cezard@ed.ac.uk>
2015-07-16 12:48:22 +0100

    Seamlessly get the shared specific parameters from the config file

--- config.py
+++ config.py
@@ -29,10 +29,15 @@ class Config():
     """
     def __init__(self):
         self.config_file = open(self.__class__._find_config_file(), 'r')
-        self.config = yaml.load(self.config_file)
+        tmp_config = yaml.load(self.config_file)
         self._environment = None
         #Select the config specific to the environment
-        self.config = self.config[self.environment]
+        self.config = tmp_config[self.environment]
+
+        #Merge the shared and the specific environment
+        self.content = dict(
+                self.__class__._merge_dicts(self.config.get('shared'), self.config.get('analysisdriver'))
+            )
 
     @property
     def environment(self):
@@ -52,14 +57,32 @@ class Config():
                 logging.info('Found config file at {}'.format(p))
                 return p
 
+    @classmethod
+    def _merge_dicts(cls, default, override):
+        """
+        Recursively merges an 'override' dictionary into a 'default' dictionary.
+
+        Adopted from http://stackoverflow.com/a/7205672/1167094
+        """
+        for k in set(default.keys()).union(override.keys()):
+            if k in default and k in override:
+                if isinstance(default[k], dict) and isinstance(override[k], dict):
+                    yield (k, dict(cls._merge_dicts(default[k], override[k])))
+                else:
+                    yield (k, override[k])
+            elif k in default:
+                yield (k, default[k])
+            else:
+                yield (k, override[k])
+
     def __getitem__(self, item):
         #Allow access to the element of the config with dictionary style
-        return self.config[item]
+        return self.content[item]
 
 
     def __getattr__(self, item):
         #Allow access to the first layer with attribute style
-        return self.config[item]
+        return self.content[item]
 
 
 
@@ -70,7 +93,7 @@ class test_config(TestCase):
         self.config=Config()
 
     def test_getitem(self):
-        self.assertEqual(self.config['shared']['raw_dir'],'raw')
+        self.assertEqual(self.config['raw_dir'],'raw')
 
     def test_getattr(self):
-        self.assertEqual(self.config.shared['raw_dir'],'raw')
+        self.assertEqual(self.config.raw_dir,'raw')
