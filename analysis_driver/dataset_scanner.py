bf0fa4c3170e131e1e2d8c399fcaaeef32aa4402
tcezard <timothee.cezard@ed.ac.uk>
2015-11-05 22:17:23 +0000

    add simple Sample scanner

--- analysis_driver/dataset_scanner.py
+++ analysis_driver/dataset_scanner.py
@@ -68,10 +68,70 @@ class DatasetScanner():
 
 class RunScanner(DatasetScanner):
 
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+
     def report(self, all_datasets=False):
         datasets = self.scan_datasets()
         out = []
-        out.append('========= Process Trigger report =========')
+        out.append('========= Run Scanner report =========')
+        out.append('dataset location: ' + self.input_dir)
+        for status in ('new', 'new, rta complete', 'transferring', 'transferring, rta complete', 'active'):
+            ds = datasets.pop(status, [])
+            if ds:
+                out.append('=== ' + status + ' ===')
+                out.append('\n'.join((os.path.basename(d) for d in ds)))
+
+        if any((datasets[s] for s in datasets)):
+            if all_datasets:
+                for status in sorted(datasets):
+                    out.append('=== ' + status + ' ===')
+                    out.append('\n'.join((os.path.basename(x) for x in datasets[status])))
+            else:
+                out.append('=== other datasets ===')
+                out.append('\n'.join(('other datasets present', 'use --report-all to show')))
+
+        out.append('_' * 42)
+
+    def dataset_status(self, dataset):
+        dataset_lock_files = glob(self.lock_file(dataset, '*'))
+        assert len(dataset_lock_files) < 2
+        if dataset_lock_files:
+            lf_status = dataset_lock_files[0].split('.')[-1]
+        else:
+            lf_status = 'new'
+
+        rta_complete = self._rta_complete(dataset)
+
+        if lf_status in ('complete', 'active'):
+            assert rta_complete
+            return lf_status
+
+        elif lf_status in ('aborted', 'failed'):
+            return lf_status
+
+        else:
+            if rta_complete:
+                lf_status += ', rta complete'
+            return lf_status
+
+
+    def _rta_complete(self, dataset):
+        return os.path.isfile(os.path.join(self.input_dir, dataset, 'RTAComplete.txt'))
+
+
+
+class SampleScanner(DatasetScanner):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+
+    def report(self, all_datasets=False):
+        datasets = self.scan_datasets()
+        out = []
+        out.append('========= Sample Scanner report =========')
         out.append('dataset location: ' + self.input_dir)
         for status in ('new', 'new, rta complete', 'transferring', 'transferring, rta complete', 'active'):
             ds = datasets.pop(status, [])
