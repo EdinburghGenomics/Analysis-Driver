5e92b333930ee69eca9ae8f0e07bb2129225ab59
tcezard <timothee.cezard@ed.ac.uk>
2015-11-05 22:14:02 +0000

    initial rewrite of the drive as two pipelines: one for demultiplexing one for variant calling

--- analysis_driver/driver.py
+++ analysis_driver/driver.py
@@ -12,7 +12,7 @@ from analysis_driver.notification import default as ntf
 app_logger = get_logger('driver')
 
 
-def pipeline(input_run_folder):
+def demultiplexing_pipeline(input_run_folder):
     """
     :param str input_run_folder: Full path to an input data directory
     :return: Exit status
@@ -27,10 +27,6 @@ def pipeline(input_run_folder):
     app_logger.info('Job dir: ' + job_dir)
 
     run_info = reader.RunInfo(input_run_folder)
-    samplesheet_csv = os.path.join(input_run_folder, 'SampleSheet.csv')
-    if not run_info.mask.barcode_len or not os.path.exists(samplesheet_csv):
-        app_logger.info('No sample sheet or barcodes found. Running in phiX mode')
-        return pipeline_phix(input_run_folder)
 
     ntf.start_stage('setup')
     reader.transform_sample_sheet(input_run_folder)
@@ -69,18 +65,76 @@ def pipeline(input_run_folder):
 
     valid_lanes = clarity.get_valid_lanes(run_info.flowcell_name)
 
+    #TODO: calculate md5 sum on the cluster
+    fastqc_exit_status = fastqc_executor.join()
+    ntf.end_stage('fastqc', fastqc_exit_status)
+    exit_status += fastqc_exit_status
+
+    #TODO: copy the stats file
+    exit_status += copy_fastq_to_output_dir(fastq_dir, run_id, sample_sheet, valid_lanes)
+
+    if exit_status == 0:
+        ntf.start_stage('cleanup')
+        exit_status += _cleanup(run_id)
+        ntf.end_stage('cleanup', exit_status)
+
+
+def copy_fastq_to_output_dir(fastq_dir, run_id, sample_sheet, valid_lanes):
+    """Retrieve and copy the fastq files to the output directory"""
+    output_dir = cfg['output_dir']
+    run_dir = os.path.join(output_dir, 'runs', run_id)
+    files_to_transfer=[]
+    for sample_project, proj_obj in sample_sheet.sample_projects.items():
+        project_dir = os.path.join(run_dir, sample_project)
+        for sample_id, id_obj in proj_obj.sample_ids.items():
+            fastq_files = []
+            if valid_lanes:
+                # only retrieve the lanes that are considered valid
+                for lane in valid_lanes:
+                    fastq_files.extend(
+                        util.fastq_handler.find_fastqs(fastq_dir, sample_project, sample_id, lane)
+                    )
+            else:
+                # no information about valid lanes: don't filter anything
+                fastq_files.extend(util.fastq_handler.find_fastqs(fastq_dir, sample_project, sample_id))
+            for fastq_file in fastq_files:
+                files_to_transfer.append(os.path.join(sample_project,sample_id,fastq_file))
+    exit_status = 0
+    for path in files_to_transfer:
+        out_dirname = os.path.join(run_dir, os.path.dirname(path))
+        os.makedirs(out_dirname, exist_ok=True)
+        in_path = os.path.join(fastq_dir, path)
+        out_path = os.path.join(run_dir, path)
+        exit_status += util.transfer_output_file(
+                        in_path,
+                        out_path
+                    )
+    return exit_status
+def variant_calling_pipeline(input_sample_folder):
+    """
+    :param str input_run_folder: Full path to an input data directory
+    :return: Exit status
+    :rtype: int
+    """
+    exit_status = 0
+    sample_id = os.path.basename(input_sample_folder)
+    sample_dir = os.path.join(cfg['jobs_dir'], sample_id)
+    app_logger.info('Job dir: ' + sample_dir)
+
+    #TODO: find fastq files that are valid base on the LIMS?
+
     # merge fastq files
     ntf.start_stage('merge fastqs')
-    sample_to_fastq_files = _bcbio_prepare_samples(run_id, fastq_dir, job_dir, sample_sheet, valid_lanes)
-    app_logger.debug('sample_to_fastq mapping: ' + str(sample_to_fastq_files))
+    sample_fastq_files = _bcbio_prepare_sample(sample_dir, sample_id, fastq_files)
+    app_logger.debug('sample fastq files: ' + str(sample_fastq_files))
     ntf.end_stage('merge fastqs')
 
     # fastqc2
-    ntf.start_stage('fastqc2')
+    ntf.start_stage('sample_fastqc2')
     fastqc2_executor = executor.execute(
-        [writer.bash_commands.fastqc(fq) for fastq_pair in sample_to_fastq_files.values() for fq in fastq_pair],
+        [writer.bash_commands.fastqc(fastq_pair) for fastq_pair in sample_fastq_files.values()],
         job_name='fastqc2',
-        run_id=run_id,
+        run_id=sample_id,
         walltime=10,
         cpus=1,
         mem=2
@@ -93,18 +147,15 @@ def pipeline(input_run_folder):
 
     # bcbio
     ntf.start_stage('bcbio')
-    bcbio_executor = _run_bcbio(run_id, job_dir, sample_to_fastq_files)
+    bcbio_executor = _run_bcbio(sample_id, sample_dir, sample_fastq_files)
 
     # wait for genotype_validation fastqc and bcbio to finish
     # genotype_results = genotype_validation.join()
     # app_logger.info('Written files: ' + str(genotype_results))
-    ntf.end_stage('genotype validation')
-
-    fastqc_exit_status = fastqc_executor.join()
-    ntf.end_stage('fastqc', fastqc_exit_status)
+    # ntf.end_stage('genotype validation')
 
     fastqc2_exit_status = fastqc2_executor.join()
-    ntf.end_stage('fastqc2', fastqc2_exit_status)
+    ntf.end_stage('sample_fastqc2', fastqc2_exit_status)
 
     bcbio_exit_status = bcbio_executor.join()
     ntf.end_stage('bcbio', bcbio_exit_status)
@@ -112,7 +163,7 @@ def pipeline(input_run_folder):
     # sort out exit statuses
     if bcbio_exit_status:
         return bcbio_exit_status
-    exit_status += fastqc_exit_status + fastqc2_exit_status + bcbio_exit_status
+    exit_status += fastqc2_exit_status + bcbio_exit_status
 
     # transfer output data
     ntf.start_stage('data_transfer')
@@ -120,95 +171,31 @@ def pipeline(input_run_folder):
     ntf.end_stage('data_transfer', transfer_exit_status)
     exit_status += transfer_exit_status
 
-    if exit_status == 0:
-        ntf.start_stage('cleanup')
-        exit_status += _cleanup(run_id)
-        ntf.end_stage('cleanup', exit_status)
-
     return exit_status
 
 
-def pipeline_phix(input_run_folder):
-    # TODO: remove this, find a better way of having alternate pipelines
-    exit_status = 0
-    run_id = os.path.basename(input_run_folder)
-    job_dir = os.path.join(cfg['jobs_dir'], run_id)
-    fastq_dir = os.path.join(job_dir, 'fastq')
-
-    # bcl2fastq
-    ntf.start_stage('bcl2fastq')
-    exit_status += executor.execute(
-        [writer.bash_commands.bcl2fastq(input_run_folder, fastq_dir)],
-        job_name='bcl2fastq',
-        run_id=run_id,
-        walltime=32,
-        cpus=8,
-        mem=32
-    ).join()
-    ntf.end_stage('bcl2fastq', exit_status)
-    if exit_status:
-        return exit_status
-
-    # fastqc
-    ntf.start_stage('fastqc')
-    exit_status += executor.execute(
-        [writer.bash_commands.fastqc(fq) for fq in util.fastq_handler.find_all_fastqs(fastq_dir)],
-        job_name='fastqc',
-        run_id=run_id,
-        walltime=6,
-        cpus=4,
-        mem=2
-    ).join()
-    ntf.end_stage('fastqc', exit_status)
-
-    return exit_status
-
-
-def _bcbio_prepare_samples(run_id, fastq_dir, job_dir, sample_sheet, valid_lanes=None):
+def _bcbio_prepare_sample(job_dir, sample_id, fastq_files):
     """
     Merge the fastq files per sample using bcbio prepare sample
     """
-    cmds = []
-    sample_fastqs = {}
+    user_sample_id = clarity.get_user_sample_name(sample_id)
+    if not user_sample_id:
+        user_sample_id = sample_id
+    cmd = util.bcbio_prepare_samples_cmd(
+            job_dir,
+            sample_id,
+            fastq_files,
+            user_sample_id=user_sample_id
+          )
 
-    for sample_project, proj_obj in sample_sheet.sample_projects.items():
-        for sample_id, id_obj in proj_obj.sample_ids.items():
-            fastq_files = []
-            if valid_lanes:
-                # only retrieve the lanes that are considered valid
-                for lane in valid_lanes:
-                    fastq_files.extend(
-                        util.fastq_handler.find_fastqs(fastq_dir, sample_project, sample_id, lane)
-                    )
-            else:
-                # no information about valid lanes: don't filter anything
-                fastq_files.extend(util.fastq_handler.find_fastqs(fastq_dir, sample_project, sample_id))
-
-            user_sample_id = clarity.get_user_sample_name(sample_id)
-            if not user_sample_id:
-                user_sample_id = sample_id
-
-            cmds.append(
-                util.bcbio_prepare_samples_cmd(
-                    job_dir,
-                    sample_id,
-                    fastq_files,
-                    user_sample_id=clarity.get_user_sample_name(sample_id)
-                )
-            )
-            sample_fastqs[sample_id] = os.path.join(job_dir, 'merged', user_sample_id + '_R?.fastq.gz')
-
-    exit_status = executor.execute(cmds, job_name='bcbio_prepare_samples', run_id=run_id).join()
-
-    for sample_id, pattern in sample_fastqs.items():
-        sample_fastqs[sample_id] = glob(pattern)
+    exit_status = executor.execute([cmd], job_name='bcbio_prepare_samples', run_id=sample_id).join()
+    sample_fastqs = glob(os.path.join(job_dir, 'merged', user_sample_id + '_R?.fastq.gz'))
 
     app_logger.info('bcbio_prepare_samples finished with exit status ' + str(exit_status))
 
     return sample_fastqs
 
-
-def _run_bcbio(run_id, job_dir, sample_name_to_fastqs):
+def _run_bcbio(sample_id, sample_dir, sample_fastqs):
     run_template = os.path.join(
         os.path.dirname(__file__),
         '..', 'etc', 'bcbio_alignment_' + cfg['genome'] + '.yaml'
@@ -219,49 +206,45 @@ def _run_bcbio(run_id, job_dir, sample_name_to_fastqs):
         )
 
     original_dir = os.getcwd()
-    os.chdir(job_dir)
-    app_logger.debug(str(sample_name_to_fastqs))
+    os.chdir(sample_dir)
+    app_logger.debug(str(sample_fastqs))
 
-    bcbio_array_cmds = []
-    for sample_name in sample_name_to_fastqs:
-        app_logger.debug('Setting up sample: ' + sample_name)
+    app_logger.debug('Setting up sample: ' + sample_id)
 
-        bcbio_dir = os.path.join(job_dir, 'samples_' + sample_name + '-merged')
+    bcbio_dir = os.path.join(sample_dir, 'samples_' + sample_id + '-merged')
 
-        sample_prep = [
-            os.path.join(cfg['bcbio'], 'bin', 'bcbio_nextgen.py'),
-            '-w template',
-            run_template,
-            bcbio_dir,
-            bcbio_dir + '.csv'
-        ] + sample_name_to_fastqs.get(sample_name)
+    sample_prep = [
+        os.path.join(cfg['bcbio'], 'bin', 'bcbio_nextgen.py'),
+        '-w template',
+        run_template,
+        bcbio_dir,
+        bcbio_dir + '.csv'
+    ] + sample_fastqs
 
-        run_yaml = os.path.join(bcbio_dir, 'config', 'samples_' + sample_name + '-merged.yaml')
-        bcbio_array_cmds.append(
-            writer.bash_commands.bcbio(
-                run_yaml,
-                os.path.join(bcbio_dir, 'work'),
-                threads=16
-            )
-        )
-        prep_status = executor.execute([' '.join(sample_prep)], env='local').join()
-        app_logger.info('BCBio sample prep exit status: ' + str(prep_status))
+    run_yaml = os.path.join(bcbio_dir, 'config', 'samples_' + sample_id + '-merged.yaml')
+    bcbio_cmd= writer.bash_commands.bcbio(
+        run_yaml,
+        os.path.join(bcbio_dir, 'work'),
+        threads=16
+    )
+    prep_status = executor.execute([' '.join(sample_prep)], env='local').join()
+    app_logger.info('BCBio sample prep exit status: ' + str(prep_status))
 
-        user_sample_id = clarity.get_user_sample_name(sample_name)
-        if user_sample_id:
-            app_logger.debug('Found user sample: ' + user_sample_id)
+    user_sample_id = clarity.get_user_sample_name(sample_id)
+    if user_sample_id:
+        app_logger.debug('Found user sample: ' + user_sample_id)
 
-            with open(run_yaml, 'r') as i:
-                run_config = yaml.load(i)
-            run_config['fc_name'] = user_sample_id
-            with open(run_yaml, 'w') as o:
-                o.write(yaml.safe_dump(run_config, default_flow_style=False))
+        with open(run_yaml, 'r') as i:
+            run_config = yaml.load(i)
+        run_config['fc_name'] = user_sample_id
+        with open(run_yaml, 'w') as o:
+            o.write(yaml.safe_dump(run_config, default_flow_style=False))
 
     bcbio_executor = executor.execute(
-        bcbio_array_cmds,
+        [bcbio_cmd],
         prelim_cmds=writer.bash_commands.bcbio_env_vars(),
-        job_name='bcbio',
-        run_id=run_id,
+        job_name='bcb%s'%sample_id,
+        run_id=sample_id,
         walltime=240,
         cpus=8,
         mem=64
