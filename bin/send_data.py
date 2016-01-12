349cfd51cc0e26dac5f532d71583f037605dbaa8
Murray Wham <murray.wham@ed.ac.uk>
2016-01-12 11:47:53 +0000

    #84: Adding stdout mode for testing

--- bin/send_data.py
+++ bin/send_data.py
@@ -2,6 +2,7 @@ __author__ = 'mwham'
 import sys
 import os
 import argparse
+import json
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from analysis_driver.reader import SampleSheet
@@ -26,22 +27,36 @@ def main():
     sample_parser.set_defaults(func=sample_crawler)
 
     args = p.parse_args()
-    c = args.func(args)
-    if args.test:
-        print(c)
-    else:
-        c.send_data()
+    args.func(args)
 
 
 def run_crawler(args):
     for f in (args.samplesheet, args.conversion_stats):
         assert os.path.isfile(f), 'Missing file: ' + f
-    return RunCrawler(args.run_id, SampleSheet(args.samplesheet), args.conversion_stats)
+    c = RunCrawler(args.run_id, SampleSheet(args.samplesheet), args.conversion_stats)
+    if args.test:
+        print(
+            json.dumps(
+                {
+                    'run_elements': list(c.barcodes_info.values()),
+                    'unexpected_barcodes': list(c.unexpected_barcodes.values()),
+                    'lanes': list(c.lanes.values()),
+                    'runs': c.run,
+                    'samples': list(c.libraries.values()),
+                    'project': list(c.projects.values())
+                },
+                indent=4
+            )
+        )
+    else:
+        c.send_data()
 
 
 def sample_crawler(args):
     assert os.listdir(args.input_dir)
-    return SampleCrawler(args.sample_id, args.project_id, args.input_dir)
+    c = SampleCrawler(args.sample_id, args.project_id, args.input_dir)
+    if args.test:
+        print(json.dumps({'samples': c.sample}, indent=4))
 
 
 if __name__ == '__main__':
