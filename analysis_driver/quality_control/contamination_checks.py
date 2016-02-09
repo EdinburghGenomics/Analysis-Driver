cf6610e080567df3aa4cc4890c8d348095df67be
Katie Emelianova <katie_emelianova@hotmail.com>
2016-02-09 16:15:13 +0000

    Added a contamination-check part #91

--- analysis_driver/quality_control/contamination_checks.py
+++ analysis_driver/quality_control/contamination_checks.py
@@ -1,4 +1,7 @@
+import os
 from analysis_driver.config import default as cfg
+from analysis_driver import executor
+from analysis_driver.notification import default as ntf
 
 
 class ContaminationCheck():
@@ -7,20 +10,64 @@ class ContaminationCheck():
         self.fastq_files = fastq_files
         self.sample_id = sample_id
         self.contamination_cfg = cfg.get('contamination-check')
+        self.workDir = self.contamination_cfg.get('kontaminant_running')
 
 
-    def _kontaminant_command(self, referenceSpecies):
+    def createOutputDirectories(self,species):
+        if os.path.exists(os.path.join(self.workDir, self.sample_id, species)) == True:
+            os.rmdir(os.path.join(self.workDir, self.sample_id, species))
+            outputDirectory = (os.path.join(self.workDir, self.sample_id, species))
+        else:
+            outputDirectory = (os.path.join(self.workDir, self.sample_id, species))
+        return outputDirectory
+
+
+
+
+    def _kontaminant_command(self, refSpecies, outputDirectory):
         """
         :param referenceSpecies: a string passed from _loop_through_references
-        takes the name of a reference species (referenceSpecies) and returns a kontnaminant command for
+        takes the name of a reference species (referenceSpecies) and returns a kontaminant command for
         read filtering using your reads and the prepared referenceSpecies kmer database
         :return str: the command used to run Kontaminant
         """
         if len(self.fastq_files) == 1:
-            kontaminant_command = ("This is my single end kontaminant command for %s" % referenceSpecies)
+
+            kontaminantBin = (self.contamination_cfg.get('kontaminant_bin').rstrip('/') + '/')
+            referenceDirectory = (self.contamination_cfg.get('kontaminant_reference_directory').rstrip('/') + '/')
+            referencePath = os.path.join(referenceDirectory,refSpecies)
+            fastq1 = self.fastq_files[0]
+
+            kontaminant_command = ("{}kmer_filter_31"
+                                   " --reference {} "
+                                   "--read1 {} "
+                                   "--output_prefix filtered_K21_Thr1 "
+                                   "--output_folder {} "
+                                   "--threashold 1".format(kontaminantBin,
+                                                           referencePath,
+                                                           fastq1,
+                                                           outputDirectory))
             return kontaminant_command
+
         elif len(self.fastq_files) == 2:
-            kontaminant_command = ("This is my paired end kontaminant command for %s" % referenceSpecies)
+
+            kontaminantBin = (self.contamination_cfg.get('kontaminant_bin').rstrip('/') + '/')
+            referenceDirectory = (self.contamination_cfg.get('kontaminant_reference_directory').rstrip('/') + '/')
+            referencePath = os.path.join(referenceDirectory,refSpecies)
+            fastq1 = self.fastq_files[0]
+            fastq2 = self.fastq_files[1]
+
+            kontaminant_command = ("{}kmer_filter_31"
+                                   " --reference {} "
+                                   "--read1 {} "
+                                   "--read2 {} "
+                                   "--output_prefix filtered_K21_Thr1 "
+                                   "--output_folder {} "
+                                   "--threashold 1".format(kontaminantBin,
+                                                           referencePath,
+                                                           fastq1,
+                                                           fastq2,
+                                                           outputDirectory))
             return kontaminant_command
         else:
            raise ValueError('Bad number of fastqs: ' + str(self.fastq_files))
@@ -29,18 +76,41 @@ class ContaminationCheck():
 
     def _loop_through_references(self):
         """
-        :return: dict: a dict of species:species specific kontaminant commands
+        :return: list: a list of species specific kontaminant commands
         """
         kontaminantReferences = self.contamination_cfg.get('kontaminant_references')
         kontaminantCommands = {}
         for species in kontaminantReferences:
-            speciesCommand = self._kontaminant_command(species)
+            outputDirectory = self.createOutputDirectories(species)
+            speciesCommand = self._kontaminant_command(species, outputDirectory)
             kontaminantCommands[species] = (speciesCommand)
         return(kontaminantCommands)
 
 
 
 
+
+
+    def _runKontaminant(self):
+        kontaminantCommands = self._loop_through_references()
+        expectedOutfiles = {}
+        commands = []
+        for species in kontaminantCommands:
+            expectedOutfile = (os.path.join(self.workDir, self.sample_id, species, 'filtered_K21_Thr1' + species + '.FASTQ',))
+            expectedOutfiles[species] = expectedOutfile
+            commands.append(kontaminantCommands[species])
+        ntf.start_stage('kontaminant_contamination_check')
+        kontaminant_executor = executor.execute(
+            commands,
+            job_name='kontaminant',
+            sample_id=self.sample_id,
+            cpus=4,
+            mem=25
+        )
+        exit_status = kontaminant_executor.join()
+        ntf.end_stage('kontaminant_contamination_check', exit_status)
+
+        return expectedOutfiles
 
 
 
--- tests/test_quality_control/test_contamination_checks.py
+++ tests/test_quality_control/test_contamination_checks.py
@@ -1,27 +1,39 @@
 from tests.test_analysisdriver import TestAnalysisDriver
 from analysis_driver.quality_control import ContaminationCheck
-from analysis_driver.exceptions import AnalysisDriverError
 from analysis_driver.config import default as cfg
+from unittest.mock import patch
 
 
 class TestContaminationCheck(TestAnalysisDriver):
+
+    def test_createOutputDirectories(self):
+        fastq_files = ['fastqFile1', 'fastqFile2']
+        sample_id = 'testSample'
+        species = 'testSpecies'
+        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
+        createOutputDirectoriesCommand = c.createOutputDirectories(species)
+        assert createOutputDirectoriesCommand =='path/to/kontaminant/working/dir/testSample/testSpecies'
+
+
+
 
     def test_kontaminant_command1(self):
         fastq_files = ['fastqFile1', 'fastqFile2']
         sample_id = 'testSample'
         referenceSp = 'testSpecies'
+        outputDir = 'testOutputDir'
         c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        kontaminantCmd = c._kontaminant_command(referenceSp)
-        assert kontaminantCmd == "This is my paired end kontaminant command for testSpecies"
+        kontaminantCmd = c._kontaminant_command(referenceSp, outputDir)
+        assert kontaminantCmd == "path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/testSpecies --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder testOutputDir --threashold 1"
 
         fastq_files = ['fastqFile1']
         c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        kontaminantCmd = c._kontaminant_command(referenceSp)
-        assert kontaminantCmd == "This is my single end kontaminant command for testSpecies"
+        kontaminantCmd = c._kontaminant_command(referenceSp, outputDir)
+        assert kontaminantCmd == "path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/testSpecies --read1 fastqFile1 --output_prefix filtered_K21_Thr1 --output_folder testOutputDir --threashold 1"
 
         fastq_files = ['fastqFile1', 'fastqFile2', 'fastqFile3']
         c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        self.assertRaises(ValueError, c._kontaminant_command,referenceSp)
+        self.assertRaises(ValueError, c._kontaminant_command,referenceSp,outputDir)
 
 
     def test_loop_through_references(self):
@@ -29,10 +41,22 @@ class TestContaminationCheck(TestAnalysisDriver):
         sample_id = 'testSample'
         c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
         referenceCommandDict = c._loop_through_references()
-        assert referenceCommandDict == {'species1':'This is my paired end kontaminant command for species1',
-                                        'species2':'This is my paired end kontaminant command for species2',
-                                        'species3':'This is my paired end kontaminant command for species3'}
+        print(referenceCommandDict)
+        assert referenceCommandDict == {'species1':'path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/species1 --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder path/to/kontaminant/working/dir/testSample/species1 --threashold 1',
+                                        'species2':'path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/species2 --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder path/to/kontaminant/working/dir/testSample/species2 --threashold 1',
+                                        'species3':'path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/species3 --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder path/to/kontaminant/working/dir/testSample/species3 --threashold 1'}
 
+    @patch('analysis_driver.executor.execute', autospec=True)
+    def test_runKontaminant(self, mocked_execute):
+        fastq_files = ['fastqFile1', 'fastqFile2']
+        sample_id = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
+        instance = mocked_execute.return_value
+        instance.join.return_value = 0
+        runContaminantCommand = c._runKontaminant()
+        assert runContaminantCommand == {'species1':"path/to/kontaminant/working/dir/testSample/species1/filtered_K21_Thr1species1.FASTQ",
+                                         'species2':"path/to/kontaminant/working/dir/testSample/species2/filtered_K21_Thr1species2.FASTQ",
+                                         'species3':"path/to/kontaminant/working/dir/testSample/species3/filtered_K21_Thr1species3.FASTQ"}
 
 
 
