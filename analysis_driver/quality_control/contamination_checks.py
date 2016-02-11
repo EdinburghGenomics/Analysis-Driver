5be0de421139fec2b19c75a94b82c5d803da35fe
katieemelianova <katie_emelianova@hotmail.com>
2016-02-11 13:59:09 +0000

    changed to run Fastq-Screen

--- analysis_driver/quality_control/contamination_checks.py
+++ analysis_driver/quality_control/contamination_checks.py
@@ -10,107 +10,57 @@ class ContaminationCheck():
         self.fastq_files = fastq_files
         self.sample_id = sample_id
         self.contamination_cfg = cfg.get('contamination-check')
-        self.workDir = self.contamination_cfg.get('kontaminant_running')
 
-
-    def createOutputDirectories(self,species):
-        if os.path.exists(os.path.join(self.workDir, self.sample_id, species)) == True:
-            os.rmdir(os.path.join(self.workDir, self.sample_id, species))
-            outputDirectory = (os.path.join(self.workDir, self.sample_id, species))
-        else:
-            outputDirectory = (os.path.join(self.workDir, self.sample_id, species))
-        return outputDirectory
-
-
-
-
-    def _kontaminant_command(self, refSpecies, outputDirectory):
+    def _fastqscreen_command(self):
         """
-        :param referenceSpecies: a string passed from _loop_through_references
-        takes the name of a reference species (referenceSpecies) and returns a kontaminant command for
-        read filtering using your reads and the prepared referenceSpecies kmer database
-        :return str: the command used to run Kontaminant
+        :return list: the command used to run fastq screen
         """
         if len(self.fastq_files) == 1:
 
-            kontaminantBin = (self.contamination_cfg.get('kontaminant_bin').rstrip('/') + '/')
-            referenceDirectory = (self.contamination_cfg.get('kontaminant_reference_directory').rstrip('/') + '/')
-            referencePath = os.path.join(referenceDirectory,refSpecies)
+            fastqscreen_bin = (self.contamination_cfg.get('fastq_screen_bin').rstrip('/') + '/')
+            confPath = (self.contamination_cfg.get('fastq_screen_conf').rstrip('/') + '/')
             fastq1 = self.fastq_files[0]
 
-            kontaminant_command = ("{}kmer_filter_31"
-                                   " --reference {} "
-                                   "--read1 {} "
-                                   "--output_prefix filtered_K21_Thr1 "
-                                   "--output_folder {} "
-                                   "--threashold 1".format(kontaminantBin,
-                                                           referencePath,
-                                                           fastq1,
-                                                           outputDirectory))
-            return kontaminant_command
+            fastqscreen_command = ["{} --aligner bowtie2 {} --conf {}".format(fastqscreen_bin,
+                                                                               fastq1,
+                                                                               confPath)]
+            return fastqscreen_command
 
         elif len(self.fastq_files) == 2:
-
-            kontaminantBin = (self.contamination_cfg.get('kontaminant_bin').rstrip('/') + '/')
-            referenceDirectory = (self.contamination_cfg.get('kontaminant_reference_directory').rstrip('/') + '/')
-            referencePath = os.path.join(referenceDirectory,refSpecies)
+            fastqscreen_bin = (self.contamination_cfg.get('fastq_screen_bin').rstrip('/') + '/')
+            confPath = (self.contamination_cfg.get('fastq_screen_conf').rstrip('/') + '/')
             fastq1 = self.fastq_files[0]
             fastq2 = self.fastq_files[1]
 
-            kontaminant_command = ("{}kmer_filter_31"
-                                   " --reference {} "
-                                   "--read1 {} "
-                                   "--read2 {} "
-                                   "--output_prefix filtered_K21_Thr1 "
-                                   "--output_folder {} "
-                                   "--threashold 1".format(kontaminantBin,
-                                                           referencePath,
-                                                           fastq1,
-                                                           fastq2,
-                                                           outputDirectory))
-            return kontaminant_command
+            fastqscreen_command = ["{} --aligner bowtie2 {} {} --conf {}".format(fastqscreen_bin,
+                                                                                fastq1,
+                                                                                fastq2,
+                                                                                confPath)]
+            return fastqscreen_command
         else:
            raise ValueError('Bad number of fastqs: ' + str(self.fastq_files))
 
+    def _get_expected_outfiles(self):
+        if len(self.fastq_files) == 1:
+            expected_outfiles = [((self.fastq_files[0]).rstrip('.fastq') + '_screen.txt')]
+            return expected_outfiles
+        elif len(self.fastq_files) == 2:
+            expected_outfiles = [((self.fastq_files[0]).rstrip('.fastq') + '_screen.txt'), ((self.fastq_files[1]).rstrip('.fastq') + '_screen.txt')]
+            return expected_outfiles
+        else:
+            raise ValueError('Bad number of fastqs: ' + str(self.fastq_files))
 
-
-    def _loop_through_references(self):
-        """
-        :return: list: a list of species specific kontaminant commands
-        """
-        kontaminantReferences = self.contamination_cfg.get('kontaminant_references')
-        kontaminantCommands = {}
-        for species in kontaminantReferences:
-            outputDirectory = self.createOutputDirectories(species)
-            speciesCommand = self._kontaminant_command(species, outputDirectory)
-            kontaminantCommands[species] = (speciesCommand)
-        return(kontaminantCommands)
-
-
-
-
-
-
-    def _runKontaminant(self):
-        kontaminantCommands = self._loop_through_references()
-        expectedOutfiles = {}
-        commands = []
-        for species in kontaminantCommands:
-            expectedOutfile = (os.path.join(self.workDir, self.sample_id, species, 'filtered_K21_Thr1' + species + '.FASTQ',))
-            expectedOutfiles[species] = expectedOutfile
-            commands.append(kontaminantCommands[species])
-        ntf.start_stage('kontaminant_contamination_check')
+    def _run_fastqscreen(self):
+        fastqscreen_run_command = self._fastqscreen_command()
+        fastqscreen_expected_outfiles = self._get_expected_outfiles()
+        ntf.start_stage('fastqscreen_contamination_check')
         kontaminant_executor = executor.execute(
-            commands,
-            job_name='kontaminant',
+            fastqscreen_run_command,
+            job_name='fastqscreen',
             sample_id=self.sample_id,
             cpus=4,
             mem=25
         )
         exit_status = kontaminant_executor.join()
-        ntf.end_stage('kontaminant_contamination_check', exit_status)
-
-        return expectedOutfiles
-
-
-
+        ntf.end_stage('fastqscreen_contamination_check', exit_status)
+        return fastqscreen_expected_outfiles
--- tests/test_quality_control/test_contamination_checks.py
+++ tests/test_quality_control/test_contamination_checks.py
@@ -6,63 +6,32 @@ from unittest.mock import patch
 
 class TestContaminationCheck(TestAnalysisDriver):
 
-    def test_createOutputDirectories(self):
-        fastq_files = ['fastqFile1', 'fastqFile2']
-        sample_id = 'testSample'
-        species = 'testSpecies'
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        createOutputDirectoriesCommand = c.createOutputDirectories(species)
-        assert createOutputDirectoriesCommand =='path/to/kontaminant/working/dir/testSample/testSpecies'
-
-
-
-
-    def test_kontaminant_command1(self):
-        fastq_files = ['fastqFile1', 'fastqFile2']
-        sample_id = 'testSample'
-        referenceSp = 'testSpecies'
-        outputDir = 'testOutputDir'
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        kontaminantCmd = c._kontaminant_command(referenceSp, outputDir)
-        assert kontaminantCmd == "path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/testSpecies --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder testOutputDir --threashold 1"
-
-        fastq_files = ['fastqFile1']
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        kontaminantCmd = c._kontaminant_command(referenceSp, outputDir)
-        assert kontaminantCmd == "path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/testSpecies --read1 fastqFile1 --output_prefix filtered_K21_Thr1 --output_folder testOutputDir --threashold 1"
-
-        fastq_files = ['fastqFile1', 'fastqFile2', 'fastqFile3']
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        self.assertRaises(ValueError, c._kontaminant_command,referenceSp,outputDir)
-
-
-    def test_loop_through_references(self):
-        fastq_files = ['fastqFile1', 'fastqFile2']
+    def test_fastqscreen_command(self):
+        fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
         sample_id = 'testSample'
         c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
-        referenceCommandDict = c._loop_through_references()
-        print(referenceCommandDict)
-        assert referenceCommandDict == {'species1':'path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/species1 --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder path/to/kontaminant/working/dir/testSample/species1 --threashold 1',
-                                        'species2':'path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/species2 --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder path/to/kontaminant/working/dir/testSample/species2 --threashold 1',
-                                        'species3':'path/to/kontaminant/bin/kmer_filter_31 --reference path/to/kontaminant/reference/databases/species3 --read1 fastqFile1 --read2 fastqFile2 --output_prefix filtered_K21_Thr1 --output_folder path/to/kontaminant/working/dir/testSample/species3 --threashold 1'}
+        my_fastq_screen_command = c._fastqscreen_command()
+        assert my_fastq_screen_command == ["path/to/fastq-screen/bin/ " \
+                                          "--aligner bowtie2 fastqFile1.fastq fastqFile2.fastq " \
+                                          "--conf path/to/fastq-screen/conf/file/"]
+
+    def test_get_expected_outfiles(self):
+        fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
+        sample_id = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
+        fastqscreen_expected_outfiles = c._get_expected_outfiles()
+        print(fastqscreen_expected_outfiles)
+        assert fastqscreen_expected_outfiles == ['fastqFile1_screen.txt', 'fastqFile2_screen.txt']
 
     @patch('analysis_driver.executor.execute', autospec=True)
-    def test_runKontaminant(self, mocked_execute):
-        fastq_files = ['fastqFile1', 'fastqFile2']
+    def test_run_fastqscreen(self, mocked_execute):
+        fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
         sample_id = 'testSample'
         c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
         instance = mocked_execute.return_value
         instance.join.return_value = 0
-        runContaminantCommand = c._runKontaminant()
-        assert runContaminantCommand == {'species1':"path/to/kontaminant/working/dir/testSample/species1/filtered_K21_Thr1species1.FASTQ",
-                                         'species2':"path/to/kontaminant/working/dir/testSample/species2/filtered_K21_Thr1species2.FASTQ",
-                                         'species3':"path/to/kontaminant/working/dir/testSample/species3/filtered_K21_Thr1species3.FASTQ"}
-
-
-
-
-
-
+        run_fastqscreen = c._run_fastqscreen()
+        assert run_fastqscreen == ['fastqFile1_screen.txt', 'fastqFile2_screen.txt']
 
 
 
