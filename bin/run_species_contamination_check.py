f8dd97000be3e1c57a99d8eca5dd3c810b1a0e89
katieemelianova <katie_emelianova@hotmail.com>
2016-02-15 09:56:08 +0000

    added lines to make run-specific work directory

--- bin/run_species_contamination_check.py
+++ bin/run_species_contamination_check.py
@@ -27,6 +27,8 @@ def _parse_args():
 
 def run_species_contamiantion_check(args):
     logger.info("Run contamination check for {}".format(str(args.fastq_files)))
+    work_dir = os.path.join(cfg['jobs_dir'], args.run_id)
+    os.makedirs(work_dir,exist_ok=True)
     species_contamination_check = ContaminationCheck(sorted(args.fastq_files),args.run_id)
     species_contamination_check.start()
     species_contamination_check.join()
