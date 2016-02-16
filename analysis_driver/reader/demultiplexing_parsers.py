ef56e0c3e90b98a38e8f97c6f75079ff97c83877
katieemelianova <katie_emelianova@hotmail.com>
2016-02-16 15:45:10 +0000

    changed sys paths

--- analysis_driver/reader/demultiplexing_parsers.py
+++ analysis_driver/reader/demultiplexing_parsers.py
@@ -5,7 +5,7 @@ import sys
 from xml.etree import ElementTree
 
 sys.path.append('../..')
-sys.path.append('..')
+#sys.path.append('..')
 
 from analysis_driver.clarity import get_species_from_sample
 
