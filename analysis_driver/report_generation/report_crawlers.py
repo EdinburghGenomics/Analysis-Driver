292eea8535bed2ffaec396c60cd4bee96033e37b
tcezard <timothee.cezard@ed.ac.uk>
2016-02-16 16:52:31 +0000

    fix bug in genotype assignation

--- analysis_driver/report_generation/report_crawlers.py
+++ analysis_driver/report_generation/report_crawlers.py
@@ -278,7 +278,7 @@ class SampleCrawler(Crawler):
             genotyping_results = parse_genotype_concordance(genotype_validation_paths[0])
             genotyping_result = genotyping_results.get(self.sample_id)
             if genotyping_result:
-                sample[ELEMENT_GENOTYPE_VALIDATION](genotyping_result)
+                sample[ELEMENT_GENOTYPE_VALIDATION]=genotyping_result
             else:
                 self.critical('Sample %s not found in file %s'%(self.sample_id, genotype_validation_paths[0]))
 
