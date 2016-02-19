444c79b931fb505a14e650c50bfa9786bde4e8e5
katieemelianova <katie_emelianova@hotmail.com>
2016-02-19 10:49:12 +0000

    amended returned variable names to match elements in init file

--- tests/test_reader/test_demultiplexing_parsers.py
+++ tests/test_reader/test_demultiplexing_parsers.py
@@ -1,7 +1,10 @@
 import os
 from analysis_driver.reader.demultiplexing_parsers import parse_seqtk_fqchk_file
 from analysis_driver.reader.demultiplexing_parsers import parse_fastqscreen_file
+from analysis_driver.reader.demultiplexing_parsers import get_fastqscreen_results
+from analysis_driver.reader.demultiplexing_parsers import get_focal_species
 from tests.test_analysisdriver import TestAnalysisDriver
+from analysis_driver.report_generation import ELEMENT_CONTAMINANT_UNIQUE_MAP, ELEMENT_PCNT_UNMAPPED_FOCAL, ELEMENT_PCNT_UNMAPPED
 from unittest.mock import patch
 __author__ = 'tcezard'
 
@@ -16,15 +19,29 @@ class Test_demultiplexing_stats(TestAnalysisDriver):
         assert lo_q == 8551190
         assert hi_q == 75199379
 
-    @patch('analysis_driver.reader.demultiplexing_parsers.get_species_from_sample', autospec=True)
-    def test_parse_fastqscreen_file(self, mocked_species):
+    def test_parse_fastqscreen_file1(self):
         testFile = os.path.join(self.assets_path, "fastqscreenTestOutput.txt")
-        sample_id = 'testSampleID'
-        mocked_species.return_value = 'Homo_sapiens'
-        result = parse_fastqscreen_file(testFile, sample_id)
-        assert result == [4, 1.09, 1.06]
+        result = parse_fastqscreen_file(testFile, 'Homo sapiens')
+        assert result == {ELEMENT_PCNT_UNMAPPED: 1.06, ELEMENT_PCNT_UNMAPPED_FOCAL: 1.09, ELEMENT_CONTAMINANT_UNIQUE_MAP: {'Gallus gallus': 1, 'Felis catus': 4, 'Bos taurus': 1, 'Ovis aries': 2, 'Mus musculus': 4}}
 
+    def test_parse_fastqscreen_file2(self):
+        testFile = os.path.join(self.assets_path, "fastqscreenTestOutput.txt")
+        result = parse_fastqscreen_file(testFile, 'Mellivora capensis')
+        assert result == [100, 100, 100]
 
+    @patch('analysis_driver.reader.demultiplexing_parsers.get_species_from_sample', autospec=True)
+    def test_get_fastqscreen_results1(self, mocked_species_sample):
+        testFile = os.path.join(self.assets_path, "fastqscreenTestOutput.txt")
+        mocked_species_sample.return_value = None
+        result = get_fastqscreen_results(testFile, 'testSampleID')
+        assert result == [100, 100, 100]
+
+    @patch('analysis_driver.reader.demultiplexing_parsers.get_species_from_sample', autospec=True)
+    def test_get_fastqscreen_results2(self, mocked_species_sample):
+        testFile = os.path.join(self.assets_path, "fastqscreenTestOutput.txt")
+        mocked_species_sample.return_value = 'Homo sapiens'
+        result = get_fastqscreen_results(testFile, 'testSampleID')
+        assert result == {ELEMENT_PCNT_UNMAPPED: 1.06, ELEMENT_PCNT_UNMAPPED_FOCAL: 1.09, ELEMENT_CONTAMINANT_UNIQUE_MAP: {'Gallus gallus': 1, 'Felis catus': 4, 'Bos taurus': 1, 'Ovis aries': 2, 'Mus musculus': 4}}
 
 
 
