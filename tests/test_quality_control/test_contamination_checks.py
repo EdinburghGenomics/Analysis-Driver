57dadfbcf54929211dd5c8f0715e1bb7a97d849b
katieemelianova <katie_emelianova@hotmail.com>
2016-02-24 14:38:38 +0000

    changed run_id to working_dir

--- tests/test_quality_control/test_contamination_checks.py
+++ tests/test_quality_control/test_contamination_checks.py
@@ -7,8 +7,8 @@ class TestContaminationCheck(TestAnalysisDriver):
 
     def test_fastqscreen_command(self):
         fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
-        sample_id = 'testSample'
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
+        working_dir = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
         my_fastq_screen_command = c._fastqscreen_command()
         assert my_fastq_screen_command == ["path/to/fastqscreen " \
                                           "--aligner bowtie2 fastqFile1.fastq fastqFile2.fastq " \
@@ -16,16 +16,16 @@ class TestContaminationCheck(TestAnalysisDriver):
 
     def test_get_expected_outfiles(self):
         fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
-        sample_id = 'testSample'
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
+        working_dir = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
         fastqscreen_expected_outfiles = c._get_expected_outfiles()
         assert fastqscreen_expected_outfiles == ['fastqFile1_screen.txt', 'fastqFile2_screen.txt']
 
     @patch('analysis_driver.executor.execute', autospec=True)
     def test_run_fastqscreen(self, mocked_execute):
         fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
-        sample_id = 'testSample'
-        c = ContaminationCheck(fastq_files=fastq_files, sample_id=sample_id)
+        working_dir = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
         instance = mocked_execute.return_value
         instance.join.return_value = 0
         run_fastqscreen = c._run_fastqscreen()
