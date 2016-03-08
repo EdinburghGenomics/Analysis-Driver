2ea455b0f40526de7892c27b157ae4da6c3403aa
tcezard <timothee.cezard@ed.ac.uk>
2016-03-08 12:04:56 +0000

    Add options to mark samples as delivered and add summary stats from the database

--- bin/deliver_reviewed_data.py
+++ bin/deliver_reviewed_data.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import logging
 import datetime
+from collections import defaultdict
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from analysis_driver.config import default as cfg, logging_default as log_cfg
@@ -36,31 +37,67 @@ other_list_files = [
 
 class DataDelivery(AppLogger):
 
-    def __init__(self):
+    def __init__(self, dry_run):
         self.all_commands = []
+        self.dry_run = dry_run
 
 
-    def get_deliverable_projects_samples(self, project_id=None):
-        project_to_samples = {}
+    def get_deliverable_projects_samples(self, project_id=None, sample_id=None):
+        project_to_samples = defaultdict(list)
         #Get sample that have been review from REST API but not marked as delivered
+        where_clause={"useable":"yes"}
         if project_id:
-            where_clause={"useable":"yes"}
-        else:
-            where_clause={"useable":"yes", "project_id":project_id}
+            where_clause["project_id"] = project_id
+        if sample_id:
+            where_clause["sample_id"] = sample_id
+
         samples = rest_communication.get_documents(
                 'samples',
                 depaginate=True,
-                embedded={"analysis_driver_procs":1},
+                embedded={"analysis_driver_procs":1, "run_elements":1},
                 where=where_clause
         )
+        self.all_samples_values =samples
         for sample in samples:
             processes = sample.get('analysis_driver_procs',[{}])
             processes.sort(key=lambda x: datetime.datetime.strptime(x.get('_created', '01_01_1970_00:00:00'), '%d_%m_%Y_%H:%M:%S'))
             if not processes[-1].get('status','new') == 'finished':
                 raise AnalysisDriverError("Reviewed sample %s not marked as finished"%sample.get('sample_id'))
             if sample.get('delivered','no') == 'no':
-                project_to_samples[sample.get(project_id)] = sample.get('sample_id')
+                project_to_samples[sample.get('project_id')].append(sample.get('sample_id'))
         return project_to_samples
+
+    def summarise_metrics_per_sample(self, project_id):
+        headers=['Project', 'Sample Id', 'User sample id', 'Read pair sequenced', 'Yield', 'Yield Q30', 'Nb reads in bam', 'mapping rate', 'properly mapped reads rate', 'duplicate rate', 'Mean coverage', 'Callable bases rate']
+        lines = []
+        for sample in self.all_samples_values:
+            if sample.get('project_id') == project_id:
+                res = [sample.get('project_id'),sample.get('sample_id'), sample.get('user_sample_id')]
+                clean_reads = sum([int(e.get('clean_reads', '0')) for e in sample.get('run_elements') if e.get('useable')=='yes'])
+                clean_bases_r1 = sum([int(e.get('clean_bases_r1', '0')) for e in sample.get('run_elements') if e.get('useable')=='yes'])
+                clean_bases_r2 = sum([int(e.get('clean_bases_r2', '0')) for e in sample.get('run_elements') if e.get('useable')=='yes'])
+                clean_q30_bases_r1 = sum(int(e.get('clean_q30_bases_r1', '0')) for e in sample.get('run_elements') if e.get('useable')=='yes')
+                clean_q30_bases_r2 = sum(int(e.get('clean_q30_bases_r2', '0')) for e in sample.get('run_elements') if e.get('useable')=='yes')
+                res.append(str(clean_reads))
+                res.append(str((clean_bases_r1+clean_bases_r2)/1000000000))
+                res.append(str((clean_q30_bases_r1+clean_q30_bases_r2)/1000000000))
+                theoritical_cov = (clean_bases_r1 + clean_bases_r2)/3200000000.0
+                tr = sample.get('bam_file_reads', 0)
+                mr = sample.get('mapped_reads', 0)
+                dr = sample.get('duplicate_reads', 0)
+                pmr = sample.get('properly_mapped_reads', 0)
+                if not tr:
+                    raise AnalysisDriverError('Sample %s has no total number of reads'%sample.get('sample_id'))
+                res.append(str(tr))
+                res.append(str(float(mr)/float(tr)*100))
+                res.append(str(float(pmr)/float(tr)*100))
+                res.append(str(float(dr)/float(tr)*100))
+                theoritical_cov = theoritical_cov*(float(mr)/float(tr))*(1-(float(dr)/float(tr)))
+                res.append(str(theoritical_cov))
+                #res.append(str(sample.get('median_coverage', '')))
+                res.append(str(sample.get('pc_callable', 0)*100))
+                lines.append('\t'.join(res))
+        return headers, lines
 
     def append_create_batch_delivery_folder(self, deliver_dir, project):
         batch = datetime.date.today().isoformat()
@@ -76,7 +113,8 @@ class DataDelivery(AppLogger):
         return sample_folder
 
     def append_move_file_to_sample_folder(self, file_to_move, sample_folder):
-        command = 'mv %s %s'%(file_to_move, sample_folder)
+        name = os.path.basename(file_to_move)
+        command = 'mv %s %s'%(file_to_move, os.path.join(sample_folder, name))
         self.all_commands.append(command)
 
     def get_list_of_file_to_move(self, sample_name):
@@ -99,13 +137,30 @@ class DataDelivery(AppLogger):
             rest_communication.patch_entry('samples',payload={'delivered': 'yes'}, id_field='sample_id', element_id=sample_name)
 
 
-    def deliver_data(self, project_ids=None, sample_ids=None):
-        output_dir = cfg.query('output_dir')
-        project_to_samples = self.get_deliverable_projects_samples(project_ids, sample_ids)
-        all_samples = []
+    def mark_only(self, project_id=None, sample_id=None):
+        project_to_samples = self.get_deliverable_projects_samples(project_id, sample_id)
+        all_samples=[]
         for project in project_to_samples:
             samples = project_to_samples.get(project)
-            batch_delivery_folder = self.create_batch_delivery_folder(output_dir, project)
+            for sample in samples:
+                all_samples.append(sample)
+        if self.dry_run:
+            self.info('Mark %s samples as delivered'%len(all_samples))
+        else:
+            self.mark_samples_as_released(all_samples)
+
+    def deliver_data(self, project_id=None, sample_id=None):
+        output_dir = cfg.query('output_dir')
+        delivery_dir = cfg.query('delivery_dir')
+        project_to_samples = self.get_deliverable_projects_samples(project_id, sample_id)
+        all_samples = []
+        project_to_delivery_folder = {}
+
+        for project in project_to_samples:
+            samples = project_to_samples.get(project)
+            self.info('deliver %s samples from %s'%(len(samples), project))
+            batch_delivery_folder = self.append_create_batch_delivery_folder(delivery_dir, project)
+            project_to_delivery_folder[project] = batch_delivery_folder
             for sample in samples:
                 origin_sample_dir = os.path.join(output_dir, project, sample)
                 if not os.path.isdir(origin_sample_dir):
@@ -114,20 +169,34 @@ class DataDelivery(AppLogger):
                 list_of_file_to_move = self.get_list_of_file_to_move(sample_name=sample)
 
                 for file_to_move in list_of_file_to_move:
-                    if not os.path.isfile(os.path.join(origin_sample_dir, file_to_move)):
+                    origin_file = os.path.join(origin_sample_dir, file_to_move)
+                    if not os.path.isfile(origin_file):
                         raise AnalysisDriverError('File %s for sample %s in project %s does not exist'%(file_to_move, sample, project))
-                    self.append_move_file_to_sample_folder(file_to_move, sample_folder)
+                    self.append_move_file_to_sample_folder(origin_file, sample_folder)
                 all_samples.append(sample)
 
         if self.dry_run:
+            print('====== Commands ======')
             print('\n'.join(self.all_commands))
+            print('====== Metrics ======')
+            for project in project_to_samples:
+                header, lines = self.summarise_metrics_per_sample(project)
+                print('\t'.join(header))
+                print('\n'.join(lines))
         else:
             self.mark_samples_as_released(all_samples)
             for command in self.all_commands:
                 exit_status = executor.execute([command], env='local').join()
                 if exit_status != 0:
                     raise AnalysisDriverError('command %s exited with status %s'%(command, exit_status))
-            # TODO: Generate csv file from database
+
+            for project in project_to_delivery_folder:
+                header, lines = self.summarise_metrics_per_sample(project)
+                summary_metrics_file = os.path.join(project_to_delivery_folder.get(project),'summary_metrics.csv')
+                with open(summary_metrics_file, 'w') as open_file:
+                    open_file.write('\t'.join(header) + '\n')
+                    open_file.write('\n'.join(lines) + '\n')
+
             # TODO: Generate project report
 
 
@@ -136,6 +205,9 @@ def main():
     p.add_argument('--dry_run', action='store_true')
     p.add_argument('--debug', action='store_true')
     p.add_argument('--work_dir', type=str, required=True)
+    p.add_argument('--mark_only', action='store_true')
+    p.add_argument('--project_id', type=str)
+    p.add_argument('--sample_id', type=str)
     args = p.parse_args()
 
     if args.debug:
@@ -143,7 +215,11 @@ def main():
         log_cfg.add_handler('stdout', logging.StreamHandler(stream=sys.stdout), logging.DEBUG)
 
     cfg.merge(cfg['sample'])
-
+    dd = DataDelivery(args.dry_run)
+    if args.mark_only:
+        dd.mark_only(project_id=args.project_id, sample_id=args.sample_id)
+    else:
+        dd.deliver_data(project_id=args.project_id, sample_id=args.sample_id)
 
 if __name__ == '__main__':
     main()
