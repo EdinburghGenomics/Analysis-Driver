fa1b355dd0906f7609967b09ae2de9f5a864b964
katieemelianova <katie_emelianova@hotmail.com>
2016-03-09 11:14:24 +0000

    merged test which checks the expected outfiles of fastqscreen and the test which checks executor was called once. Duplicated tests 'test_run_fastqscreen' and 'test_fastq_screen_command' to test for both paired end and single end fastq file entries.

--- tests/test_quality_control/test_contamination_checks.py
+++ tests/test_quality_control/test_contamination_checks.py
@@ -5,14 +5,23 @@ from unittest.mock import patch
 
 class TestContaminationCheck(TestAnalysisDriver):
 
-    def test_fastqscreen_command(self):
+    def test_fastqscreen_command_pairedend(self):
         fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
         working_dir = 'testSample'
         c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
         my_fastq_screen_command = c._fastqscreen_command()
-        assert my_fastq_screen_command == ["path/to/fastqscreen " \
+        assert my_fastq_screen_command == ("path/to/fastqscreen " \
                                           "--aligner bowtie2 fastqFile1.fastq fastqFile2.fastq " \
-                                          "--conf path/to/fastqscreen/conf --force"]
+                                          "--conf path/to/fastqscreen/conf --force")
+
+    def test_fastqscreen_command_singleend(self):
+        fastq_files = ['fastqFile1.fastq']
+        working_dir = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
+        my_fastq_screen_command = c._fastqscreen_command()
+        assert my_fastq_screen_command == ("path/to/fastqscreen " \
+                                          "--aligner bowtie2 fastqFile1.fastq " \
+                                          "--conf path/to/fastqscreen/conf --force")
 
     def test_get_expected_outfiles(self):
         fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
@@ -22,7 +31,7 @@ class TestContaminationCheck(TestAnalysisDriver):
         assert fastqscreen_expected_outfiles == ['fastqFile1_screen.txt', 'fastqFile2_screen.txt']
 
     @patch('analysis_driver.executor.execute', autospec=True)
-    def test_run_fastqscreen(self, mocked_execute):
+    def test_run_fastqscreen_pairedend(self, mocked_execute):
         fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
         working_dir = 'testSample'
         c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
@@ -30,15 +39,6 @@ class TestContaminationCheck(TestAnalysisDriver):
         instance.join.return_value = 0
         run_fastqscreen = c._run_fastqscreen()
         assert run_fastqscreen == ['fastqFile1_screen.txt', 'fastqFile2_screen.txt']
-
-    @patch('analysis_driver.executor.execute', autospec=True)
-    def test_run_fastqscreen(self, mocked_execute):
-        fastq_files = ['fastqFile1.fastq', 'fastqFile2.fastq']
-        working_dir = 'testSample'
-        c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
-        instance = mocked_execute.return_value
-        instance.join.return_value = 0
-        run_fastqscreen = c._run_fastqscreen()
         mocked_execute.assert_called_once_with(['path/to/fastqscreen '
                                                 '--aligner bowtie2 fastqFile1.fastq fastqFile2.fastq '
                                                 '--conf path/to/fastqscreen/conf --force'],
@@ -46,6 +46,24 @@ class TestContaminationCheck(TestAnalysisDriver):
                                                mem=10,
                                                cpus=2,
                                                job_name='fastqscreen')
+
+    @patch('analysis_driver.executor.execute', autospec=True)
+    def test_run_fastqscreen_singleend(self, mocked_execute):
+        fastq_files = ['fastqFile1.fastq']
+        working_dir = 'testSample'
+        c = ContaminationCheck(fastq_files=fastq_files, working_dir=working_dir)
+        instance = mocked_execute.return_value
+        instance.join.return_value = 0
+        run_fastqscreen = c._run_fastqscreen()
+        assert run_fastqscreen == ['fastqFile1_screen.txt']
+        mocked_execute.assert_called_once_with(['path/to/fastqscreen '
+                                                '--aligner bowtie2 fastqFile1.fastq '
+                                                '--conf path/to/fastqscreen/conf --force'],
+                                               working_dir='testSample',
+                                               mem=10,
+                                               cpus=2,
+                                               job_name='fastqscreen')
+
 
 
 
