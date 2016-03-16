df43a17ca1346254d4ea0572f8bc832de443672c
Murray Wham <murray.wham@ed.ac.uk>
2016-03-16 16:28:25 +0000

    #121: Starting FastqDeleter.delete_data

--- bin/delete_data.py
+++ bin/delete_data.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from analysis_driver.exceptions import AnalysisDriverError
 from analysis_driver.config import default as cfg, logging_default as log_cfg
 from analysis_driver.app_logging import AppLogger
-from analysis_driver import rest_communication, executor, clarity
+from analysis_driver import rest_communication, executor, clarity, util
 
 
 class Deleter(AppLogger):
@@ -40,22 +40,23 @@ class RawDataDeleter(Deleter):
     deletable_sub_dirs = ('Data', 'Logs', 'Thumbnail_Images')
 
     def deletable_runs(self):
-        non_deleted_runs = rest_communication.get_documents(
+        runs = rest_communication.get_documents(
             'runs',
             depaginate=True,
             max_results=100,
             embedded={'run_elements': 1, 'analysis_driver_procs': 1},
+            sort='run_id',
             aggregate=True
         )
 
         deletable_runs = []
-        for r in non_deleted_runs:
+        for r in runs:
             review_statuses = r.get('review_statuses')
             most_recent_proc = r.get('analysis_driver_procs', [{}])[-1]
             if type(review_statuses) is list:
                 review_statuses = [s for s in review_statuses if s]
             if review_statuses and 'not reviewed' not in review_statuses:
-                if most_recent_proc.get('status') in ('finished', 'aborted'):
+                if most_recent_proc.get('status') in ('finished', 'aborted'):  # i.e. not 'deleted'
                     deletable_runs.append(r)
 
         return deletable_runs[:self.deletion_limit]
@@ -109,10 +110,15 @@ class RawDataDeleter(Deleter):
         run_to_be_archived = p_join(cfg['input_dir'], run_id)
         self.debug('Archiving ' + run_id)
         assert not any([d in self.deletable_sub_dirs for d in os.listdir(run_to_be_archived)])
-        self._execute('mv %s %s' % (p_join(cfg['input_dir'], run_id), p_join(cfg['archive_dir'], run_id)))
+        self._execute(
+            'mv %s %s' % (
+                p_join(cfg['input_dir'], run_id),
+                p_join(cfg['data_deletion']['raw_archives'], run_id)
+            )
+        )
         self.debug('Archiving done')
 
-    def run_deletion(self):
+    def delete_data(self):
         deletable_runs = self.deletable_runs()
         self.debug(
             'Found %s runs for deletion: %s' % (len(deletable_runs), [r['run_id'] for r in deletable_runs])
@@ -132,7 +138,7 @@ class RawDataDeleter(Deleter):
             assert run['run_id'] in runs_to_delete
             self.mark_run_as_deleted(run)
             self.archive_run(run['run_id'])
-            assert os.listdir(p_join(cfg['archive_dir'], run['run_id']))
+            assert os.listdir(p_join(cfg['data_deletion']['raw_archives'], run['run_id']))
 
         self.delete_runs(deletion_dir)
 
@@ -148,21 +154,91 @@ class FastqDeleter(Deleter):
     def samples_released_in_lims(self):
         if self._samples_released_in_lims is None:
             self._samples_released_in_lims = clarity.get_released_samples()
-        return self._samples_released_in_lims
+        return set(self._samples_released_in_lims)
 
     @property
     def samples_released_in_app(self):
         if self._samples_released_in_app is None:
             self._samples_released_in_app = rest_communication.get_documents(
                 'samples',
-                where={'delivered': 'yes', 'useable': 'yes'},
+                where={'delivered': 'yes', 'useable': 'yes'},  # TODO: do we want useable only?
                 projection={'sample_id': 1},
                 depaginate=True
             )
-        return sorted(set([s['sample_id'] for s in self._samples_released_in_app]))
+        return set([s['sample_id'] for s in self._samples_released_in_app])
 
-    def deletable_fastqs(self):
+    @staticmethod
+    def find_run_elements_for_sample(sample_id):
+        return rest_communication.get_documents('run_elements', where={'sample_id': sample_id})
+
+    @staticmethod
+    def find_fastqs_for_run_element(run_element):
+        return util.find_fastqs(
+            p_join(cfg['data_deletion']['fastqs'], run_element['run_id'], 'fastq'),
+            run_element['project_id'],
+            run_element['sample_id'],
+            lane=run_element['lane']
+        )
+
+    def deletable_samples(self):
+        return self.samples_released_in_lims & self.samples_released_in_app
+
+    def find_all_deletable_fastqs(self):
+        fastqs = []
+        for sample_id in self.deletable_samples():
+            for e in self.find_run_elements_for_sample(sample_id):
+                fastqs.extend(self.find_fastqs_for_run_element(e))
+
+        return fastqs
+
+    def setup_fastqs_for_deletion(self):
         pass
+
+    def delete_data(self):
+        deletable_samples = self.deletable_samples()
+        samples_to_delete = {}
+
+        n_samples = 0
+        n_run_elements = 0
+        n_fastqs = 0
+
+        for s in deletable_samples:
+            n_samples += 1
+            samples_to_delete[s] = {}
+            for e in self.find_run_elements_for_sample(s):
+                n_run_elements += 1
+                fastqs = self.find_fastqs_for_run_element(e)
+                n_fastqs += len(e)
+                samples_to_delete[s][e['run_element_id']] = fastqs
+
+        print(samples_to_delete)
+
+        #self.debug(
+        print(
+            'Found %s deletable fastqs from %s samples and %s run elements' % (
+                n_fastqs, n_samples, n_run_elements
+            )
+        )
+
+        if self.dry_run or not n_fastqs:
+            return 0
+
+        # deletion_dir = self.setup_fastqs_for_deletion(deletable_fastqs)
+        # fastqs_to_delete = os.listd
+        # runs_to_delete = os.listdir(deletion_dir)
+        # self._compare_lists(
+        #     runs_to_delete,
+        #     [run['run_id'] for run in deletable_runs]
+        # )
+        # assert all([os.listdir(p_join(cfg['input_dir'], r)) for r in runs_to_delete])
+        #
+        # for run in deletable_runs:
+        #     assert run['run_id'] in runs_to_delete
+        #     self.mark_run_as_deleted(run)
+        #     self.archive_run(run['run_id'])
+        #     assert os.listdir(p_join(cfg['archive_dir'], run['run_id']))
+        #
+        # self.delete_runs(deletion_dir)
 
 
 def main():
