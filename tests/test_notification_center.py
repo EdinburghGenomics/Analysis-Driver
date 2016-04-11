8ac80215d6d53674211a2ef4674921c1b0c381e9
Murray Wham <murray.wham@ed.ac.uk>
2016-04-11 13:59:05 +0100

    Fixing tests, removing sleep calls

--- tests/test_notification_center.py
+++ tests/test_notification_center.py
@@ -8,7 +8,6 @@ from analysis_driver.notification import EmailNotification, LogNotification
 from analysis_driver.config import default as cfg
 from analysis_driver.exceptions import AnalysisDriverError
 from tests.test_analysisdriver import TestAnalysisDriver
-from tests.test_dataset_scanner import patched_request
 
 
 class FakeSMTP(Mock):
@@ -31,12 +30,11 @@ class FakeSMTP(Mock):
 class TestNotificationCenter(TestAnalysisDriver):
     def setUp(self):
         base_dir = os.path.join(self.assets_path, 'dataset_scanner')
-        with patched_request:
-            dataset = RunDataset(
-                name='test_run_id',
-                path=os.path.join(base_dir, 'that'),
-                use_int_dir=False
-            )
+        dataset = RunDataset(
+            name='test_run_id',
+            path=os.path.join(base_dir, 'that'),
+            use_int_dir=False
+        )
         self.notification_center = NotificationCenter()
         self.notification_center.add_subscribers(
             (LogNotification, dataset, cfg.query('notification', 'log_notification')),
@@ -57,7 +55,8 @@ class TestNotificationCenter(TestAnalysisDriver):
         self.email_ntf = EmailNotification(dataset, email_config)
 
     @patch('smtplib.SMTP', new=FakeSMTP)
-    def test_retries(self):
+    @patch('analysis_driver.notification.email_notification.sleep')
+    def test_retries(self, mocked_sleep):
         assert self.email_ntf._try_send(self.email_ntf._prepare_message('this is a test')) is True
         assert self.email_ntf._try_send(self.email_ntf._prepare_message('dodgy')) is False
 
