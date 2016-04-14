9c52499f1c0512551b29e769665ee6b41e538615
katieemelianova <katie_emelianova@hotmail.com>
2016-04-14 14:27:49 +0100

    merged samtools depth command and awk to get histogram into one line

--- analysis_driver/quality_control/median_coverage.py
+++ analysis_driver/quality_control/median_coverage.py
@@ -14,9 +14,8 @@ class SamtoolsDepth(QualityControl):
     def _get_samtools_depth_command(self):
         samtools_bin = cfg['tools']['samtools']
         samtools_depth_out_file = self.bam_file.rstrip('bam') + 'depth'
-        samtools_depth_command = "%s depth -a -a -q 0 -Q 0 %s > %s" % (samtools_bin, self.bam_file, samtools_depth_out_file)
+        samtools_depth_command = """%s depth -a -a -q 0 -Q 0 %s | awk -F "\t" '{array[$1"\t"$3]+=1} END{for (val in array){print val"\t"array[val]}}' | sort -k 1,1 -nk 2,2 > %s""" % (samtools_bin, self.bam_file, samtools_depth_out_file)
         return samtools_depth_command, samtools_depth_out_file
-
 
 
     def _run_samtools_depth(self):
@@ -36,31 +35,31 @@ class SamtoolsDepth(QualityControl):
         ntf.end_stage('run_samtools_depth', exit_status)
         return samtools_depth_out_file
 
-    def _get_depth_histogram_command(self):
-        samtools_depth_outfile = self._run_samtools_depth()
-        depth_histogram_outfile = samtools_depth_outfile.rstrip('depth') + 'hist'
-        depth_histogram_command = """awk -F "\t" '{array[$1"\t"$3]+=1} END{for (val in array){print val"\t"array[val]}}' %s  | sort -k 1,1 -nk 2,2 > %s""" % (samtools_depth_outfile, depth_histogram_outfile)
-        return depth_histogram_command, depth_histogram_outfile
+    #def _get_depth_histogram_command(self):
+    #    samtools_depth_outfile = self._run_samtools_depth()
+    #    depth_histogram_outfile = samtools_depth_outfile.rstrip('depth') + 'hist'
+    #    depth_histogram_command = """awk -F "\t" '{array[$1"\t"$3]+=1} END{for (val in array){print val"\t"array[val]}}' %s  | sort -k 1,1 -nk 2,2 > %s""" % (samtools_depth_outfile, depth_histogram_outfile)
+    #    return depth_histogram_command, depth_histogram_outfile
 
-    def _run_depth_histogram_command(self):
-        depth_histogram_command, depth_histogram_outfile = self._get_depth_histogram_command()
-        ntf.start_stage('run_depth_histogram')
-        depth_histogram_executor = executor.execute(
-            [depth_histogram_command],
-            job_name='depthhistogram',
-            working_dir=self.working_dir,
-            cpus=1,
-            mem=10
-        )
-        exit_status = depth_histogram_executor.join()
-        ntf.end_stage('run_depth_histogram', exit_status)
-        return depth_histogram_outfile
+    #def _run_depth_histogram_command(self):
+    #    depth_histogram_command, depth_histogram_outfile = self._get_depth_histogram_command()
+    #    ntf.start_stage('run_depth_histogram')
+    #    depth_histogram_executor = executor.execute(
+    #        [depth_histogram_command],
+    #        job_name='depthhistogram',
+    #        working_dir=self.working_dir,
+    #        cpus=1,
+    #        mem=10
+    #    )
+    #    exit_status = depth_histogram_executor.join()
+    #    ntf.end_stage('run_depth_histogram', exit_status)
+    #    return depth_histogram_outfile
 
 
 
     def run(self):
         try:
-            self.median_coverage_expected_outfiles = self._run_depth_histogram_command()
+            self.median_coverage_expected_outfiles = self._run_samtools_depth()
         except Exception as e:
             self.exception = e
 
