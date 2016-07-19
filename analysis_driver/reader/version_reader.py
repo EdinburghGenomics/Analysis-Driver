027ba279b5e6418c5b22d60b92cb457ed3b8070a
tcezard <timothee.cezard@ed.ac.uk>
2016-07-19 14:18:51 +0100

    add class to read version of tools from the command line or from the config file.

---
+++ analysis_driver/reader/version_reader.py
@@ -0,0 +1,60 @@
+import subprocess
+
+from analysis_driver.exceptions import AnalysisDriverError
+from analysis_driver.config import default as cfg
+
+
+class Version_reader():
+    tool_name = None
+    command = None
+    def __init__(self, tool_name, command):
+        self.tool_name = tool_name
+        self.command = command
+
+    def _get_stdout_from_command(self):
+        if self.command:
+            tool = cfg.query('tools', self.tool_name)
+            p = subprocess.Popen(tool + self.command, stdout=subprocess.PIPE)
+            stdout = p.stdout.read()
+            p.wait()
+            p.stdout.close()
+            return stdout
+
+    def get_version_from_config(self):
+        return cfg.query('versions', self.tool_name)
+
+    def get_version(self):
+        version = self._get_stdout_from_command()
+        if not version:
+            version = self.get_version_from_config()
+        if version:
+            return version
+        raise AnalysisDriverError('Version of ' + self.tool_name + ' cannot be resolved')
+
+command1 = ' 2>&1 | grep "Version" | cut -d " " -f 2'
+
+all_commands = {
+    'bwa': command1,
+    'samtools': command1,
+    'bcl2fastq': ' -v 2>&1 | grep "bcl2fastq" | cut -d " " -f 2 | head -n1 ',
+    'fastqc': ' -v 2>&1  | cut -d " " -f 2 ',
+    'bcbio': ' -v',
+    'seqtk': command1,
+    'samblaster': ' -h 2>&1 | grep "Version" | cut -d " " -f 3',
+    'sambamba': ' -v 2>&1 | grep "sambamba" | cut -d " " -f 2 | head -n1',
+    'bamtools': ' -v 2>&1 | grep "bamtools" | cut -d " " -f 2 | head -n1',
+    'gatk': ' -h 2>&1 | grep "The Genome Analysis Toolkit (GATK)" | cut -d " " -f 6 | cut -d "," -f 1',
+    'bcftools': ' -v 2>&1 | grep "bcftools" | cut -d " " -f 2 | head -n1',
+    'tabix': command1,
+    'fastqscreen':' -v 2>&1 | grep "fastq_screen" | cut -d " " -f 2 | head -n1',
+    'sickle': ' --version | grep "sickle" | cut -d " " -f 3 | head -n1',
+    'verifybamid': ' 2>&1 | grep "verifyBamID" | cut -d " " -f 2 | head -n1',
+    'well_duplicate': None
+}
+
+
+def get_versions():
+    all_versions = {}
+    for tool in cfg['tools']:
+        all_versions[tool] = Version_reader(tool, all_commands.get(tool)).get_version()
+    return all_versions
---
+++ tests/test_reader/test_version_reader.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+
+from analysis_driver.reader.version_reader import Version_reader, get_versions
+from analysis_driver.config import default as cfg
+from tests.test_analysisdriver import TestAnalysisDriver
+
+
+class TestVersion(TestAnalysisDriver):
+
+    def test_get_version(self ):
+        with patch('analysis_driver.reader.version_reader.Version_reader._get_stdout_from_command', return_value='1.2'):
+            reader = Version_reader(tool_name='bwa', command=' -h | grep "Version')
+            assert reader.get_version() == '1.2'
+
+        reader = Version_reader(tool_name='bwa', command=None)
+        assert reader.get_version() == '0.9'
+
+    @patch('analysis_driver.reader.version_reader.Version_reader._get_stdout_from_command', return_value='1.2')
+    def test_get_versions(self, mocked_get_stdout):
+        versions = get_versions()
+        assert len(versions) == len(cfg['tools'])
