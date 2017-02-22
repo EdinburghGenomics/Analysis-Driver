ccd4b4adc48ade67d225a4bd3d84130e5f02cebd
tcezard <timothee.cezard@ed.ac.uk>
2017-02-22 15:44:44 +0000

    Generate samtools stats instead of bamtools stats #201

--- analysis_driver/util/bash_commands.py
+++ analysis_driver/util/bash_commands.py
@@ -124,6 +124,12 @@ def bamtools_stats(bam_file, output_file):
     app_logger.debug('Writing: ' + cmd)
     return cmd
 
+def samtools_stats(bam_file, output_file):
+    samtools_bin = cfg.query('tools', 'bamtools')
+    cmd = '%s stats %s > %s' % (samtools_bin, bam_file, output_file)
+    app_logger.debug('Writing: ' + cmd)
+    return cmd
+
 
 def md5sum(input_file):
     cmd = cfg.query('tools', 'md5sum', ret_default='md5sum') + ' %s > %s.md5' % (input_file, input_file)
