c2081e0953f6dfe1190f3158ba47d74ede6cc68c
tcezard <timothee.cezard@ed.ac.uk>
2017-05-16 15:11:02 +0100

    Make tiles filtered plural

--- analysis_driver/report_generation/report_crawlers.py
+++ analysis_driver/report_generation/report_crawlers.py
@@ -227,7 +227,7 @@ class RunCrawler(Crawler):
                 )
             if fastqfilter_stats_file == 1:
                 stats = dm.parse_fastqFilterer_stats(fastqfilter_stats_file)
-                if 'remove_tiles' in stats: barcode_info[ELEMENT_TILE_FILTERED] = stats['remove_tiles']
+                if 'remove_tiles' in stats: barcode_info[ELEMENT_TILES_FILTERED] = stats['remove_tiles']
                 if 'trim_r1' in stats: barcode_info[ELEMENT_TRIM_R1_LENGTH] = stats['trim_r1']
                 if 'trim_r2' in stats: barcode_info[ELEMENT_TRIM_R2_LENGTH] = stats['trim_r2']
             else:
