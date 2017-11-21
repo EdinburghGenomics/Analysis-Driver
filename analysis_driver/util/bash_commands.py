db2ad51e476d131918a26a6368f188de9019124a
tcezard <timothee.cezard@ed.ac.uk>
2017-11-21 11:41:25 +0000

    remove duplicate program name for picard commands

--- analysis_driver/util/bash_commands.py
+++ analysis_driver/util/bash_commands.py
@@ -191,14 +191,14 @@ def picard_command(program, tmp_dir, memory):
 
 def picard_mark_dup_command(input_file, output_file, metrics_file):
     tmp_dir = os.path.dirname(input_file)
-    cmd = '%s MarkDuplicates INPUT=%s OUTPUT=%s METRICS_FILE=%s ASSUME_SORTED=true '\
+    cmd = '%s INPUT=%s OUTPUT=%s METRICS_FILE=%s ASSUME_SORTED=true '\
           'OPTICAL_DUPLICATE_PIXEL_DISTANCE=100 VALIDATION_STRINGENCY=LENIENT'
     return cmd % (picard_command('MarkDuplicates', tmp_dir, 10), input_file, output_file, metrics_file)
 
 
 def picard_insert_size_command(input_file, metrics_file, histogram_file):
     tmp_dir = os.path.dirname(input_file)
-    cmd = '%s CollectInsertSizeMetrics INPUT=%s OUTPUT=%s HISTOGRAM_FILE=%s ASSUME_SORTED=true '\
+    cmd = '%s INPUT=%s OUTPUT=%s HISTOGRAM_FILE=%s ASSUME_SORTED=true '\
           'VALIDATION_STRINGENCY=LENIENT'
     return cmd % (picard_command('CollectInsertSizeMetrics', tmp_dir, 8), input_file, metrics_file, histogram_file)
 
