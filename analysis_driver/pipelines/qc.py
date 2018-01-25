49d61cde7a8fc7aa9167bdbf6fd36959541e028f
katieemelianova <katie_emelianova@hotmail.com>
2018-01-25 13:29:10 +0000

    Use instance of GATKStage class to get properties from it

--- analysis_driver/pipelines/qc.py
+++ analysis_driver/pipelines/qc.py
@@ -10,12 +10,13 @@ def build_pipeline(dataset):
     def stage(cls, **params):
         return cls(dataset=dataset, **params)
 
+    g = v.GATKStage()
     bam_file_production = common.build_bam_file_production(dataset)
-    haplotype = stage(v.HaplotypeCaller, input_bam=v.GATKStage.sorted_bam, previous_stages=bam_file_production)
+    haplotype = stage(v.HaplotypeCaller, input_bam=g.sorted_bam, previous_stages=bam_file_production)
     genotype = stage(v.GenotypeGVCFs, previous_stages=[haplotype])
-    select_snp = stage(v.SelectVariants, command=v.GATKStage.select_snp_command, previous_stages=[genotype])
-    filter_snp = stage(v.VariantFiltration, command=v.GATKStage.filter_snp_command, previous_stages=[select_snp])
-    vcfstats = stage(qc.VCFStats, vcf_file=v.GATKStage.filter_snp_vcf, previous_stages=[filter_snp])
+    select_snp = stage(v.SelectVariants, command=g.select_snp_command, previous_stages=[genotype])
+    filter_snp = stage(v.VariantFiltration, command=g.filter_snp_command, previous_stages=[select_snp])
+    vcfstats = stage(qc.VCFStats, vcf_file=g.filter_snp_vcf, previous_stages=[filter_snp])
     bgzip = stage(v.BGZip, previous_stages=[vcfstats])
     tabix = stage(v.Tabix, previous_stages=[bgzip])
     data_output = common.SampleDataOutput(dataset=dataset, previous_stages=[tabix], output_fileset='non_human_qc')
